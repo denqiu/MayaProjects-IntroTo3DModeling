714754f31beb52f3307247bc4c6958f0c21688a0
Dennis Qiu <dennisqiu@yahoo.com>
2021-12-30 17:45:16 -0500

    Added continued projects created during December 2019.

---
+++ Winterbreak 2019/DennisQiuRoomProject1/images/Export-Layers-as-PNG.jsx
@@ -0,0 +1,311 @@
+﻿//Author: Matthew Ericson, 2010
+//Modified by Dennis Qiu, 1-2-2020
+
+var docRef = app.activeDocument,  
+layersToExclude = ["nyt_png_info", "front background", "laptop acer", "explore", "acer text", "background", "acerScreen", "acer"], 
+layersToExport = docRef.layers.length - layersToExclude.length - countUnlockedLayers();
+ 
+var nyt_png_exporter = {
+  
+ png_info_lyr:    null,
+ 
+ prefix:         null,
+ base_path:      null,
+ transparency:   false,
+ 
+ png8:           null,
+ png24:          null,
+ 
+ dlg:            null,
+ prefs_xml:      null,
+
+ num_artboards:   0,
+ num_to_export:   0,
+    
+ init: function() {
+
+    var parse_success = false;
+
+    // find existing layers or add new one
+    try {
+      this.png_info_lyr = docRef.layers.getByName( 'nyt_png_info' );
+    } catch ( e ) {
+      
+      this.png_info_lyr = docRef.layers.add();
+      this.png_info_lyr.name = 'nyt_png_info';
+
+      //var nyt_png_info_xml = nyt_png_info.textFrames.add();
+      var nyt_png_info_xml = this.png_info_lyr.textFrames.add();
+      
+      var saved_data = new XML( '<nyt_prefs></nyt_prefs>' );
+      saved_data.appendChild( new XML('<nyt_prefix></nyt_prefix>') );
+      saved_data.appendChild( new XML('<nyt_path>~/Desktop</nyt_path>') );
+      saved_data.appendChild( new XML('<nyt_transparency>true</nyt_transparency>') );
+      saved_data.appendChild( new XML('<nyt_png8>false</nyt_png8>') );
+      saved_data.appendChild( new XML('<nyt_png24>true</nyt_png24>') );
+      
+      nyt_png_info_xml.contents = saved_data.toXMLString();    
+      
+      this.png_info_lyr.printable = false;
+      this.png_info_lyr.visible = false;
+    }
+
+
+    // get xml out of the 1 text item on that layer and parse it
+    if ( this.png_info_lyr.textFrames.length != 1 ) {
+      Window.alert( 'Please delete the nyt_png_info layer and try again.' );
+      
+    } else {     
+      
+      try {
+        this.prefs_xml     = new XML( this.png_info_lyr.textFrames[0].contents );
+        this.prefix        = this.prefs_xml.nyt_prefix;
+        this.base_path     = //this.prefs_xml.nyt_path;
+                                        docRef.path + "/laptop";
+        //this.transparency  = this.prefs_xml.nyt_transparency == "true" ? true : false;
+        this.png8          = this.prefs_xml.nyt_png8 == "true" ? true : false;
+        this.png24         = this.prefs_xml.nyt_png24 == "true" ? true : false;       
+        
+        
+        parse_success = true;
+      } catch ( e ) {
+        Window.alert( 'Please delete the this.png_info_lyr layer and try again.' );
+      }
+    }
+
+    
+    if (parse_success) {
+      this.show_dialog();
+    }
+  },
+
+
+ // dialog display
+ show_dialog: function() {
+
+    // figure out if there is stuff to process
+    this.num_artboards = docRef.layers.length;
+    this.num_to_export = this.hide_all_layers();
+    
+    // Export dialog
+    this.dlg = new Window('dialog', 'Export Artboards'); 
+    
+    // PANEL to hold options
+    this.dlg.msgPnl = this.dlg.add('panel', undefined, 'Export Artboards as PNGs'); 
+
+    // PREFIX GRP
+    var prefixGrp = this.dlg.msgPnl.add('group', undefined, '')
+    prefixGrp.oreintation = 'row';
+    prefixGrp.alignment = [ScriptUI.Alignment.LEFT, ScriptUI.Alignment.TOP]
+
+
+    var prefixSt = prefixGrp.add('statictext', undefined, 'File prefix:'); 
+    prefixSt.size = [100,20]
+
+    var prefixEt = prefixGrp.add('edittext', undefined, this.prefix); 
+    prefixEt.size = [ 300,20 ];
+
+
+    // DIR GROUP
+    var dirGrp = this.dlg.msgPnl.add( 'group', undefined, '') 
+    dirGrp.orientation = 'row'
+    dirGrp.alignment = [ScriptUI.Alignment.LEFT, ScriptUI.Alignment.TOP]
+
+    var dirSt = dirGrp.add('statictext', undefined, 'Output directory:'); 
+    dirSt.size = [ 100,20 ];
+
+
+    var dirEt = dirGrp.add('edittext', undefined, this.base_path); 
+    dirEt.size = [ 300,20 ];
+
+    var chooseBtn = dirGrp.add('button', undefined, 'Choose ...' );
+    chooseBtn.onClick = function() { dirEt.text = Folder.selectDialog(); }
+
+    this.dlg.transPnl = this.dlg.msgPnl.add('group', undefined, ''); 
+    this.dlg.transPnl.orientation = 'row'
+    this.dlg.transPnl.alignment = [ScriptUI.Alignment.LEFT, ScriptUI.Alignment.TOP]
+
+
+    this.dlg.transPnl.transparentChk = this.dlg.transPnl.add('checkbox', undefined, 'Transparency');
+    this.dlg.transPnl.transparentChk.value = this.transparency;
+
+    this.dlg.transPnl.png8Btn = this.dlg.transPnl.add('radiobutton', undefined, 'PNG 8');
+    this.dlg.transPnl.png8Btn.value = this.png8;
+
+    this.dlg.transPnl.png24Btn = this.dlg.transPnl.add('radiobutton', undefined, 'PNG 24');
+    this.dlg.transPnl.png24Btn.value = this.png24;
+
+    this.dlg.progBar = this.dlg.msgPnl.add( 'progressbar', undefined, 0, 100 );
+    this.dlg.progBar.size = [400,10]
+
+    this.dlg.progLabel = this.dlg.msgPnl.add('statictext', undefined, 'Will export ' + this.num_to_export + ' of ' + this.num_artboards + ' layers in document' ); 
+    this.dlg.progLabel.size = [ 400,20 ];
+
+    this.dlg.btnPnl = this.dlg.add('group', undefined, ''); 
+    this.dlg.btnPnl.orientation = 'row'
+
+    this.dlg.btnPnl.cancelBtn = this.dlg.btnPnl.add('button', undefined, 'Cancel', {name:'cancel'});
+    this.dlg.btnPnl.cancelBtn.onClick = function() { nyt_png_exporter.dlg.close() };
+
+
+    this.dlg.btnPnl.okBtn = this.dlg.btnPnl.add('button', undefined, 'Export', {name:'ok'});
+
+    this.dlg.btnPnl.okBtn.onClick = function() { 
+        
+        nyt_png_exporter.prefix       = prefixEt.text; 
+        nyt_png_exporter.base_path    = dirEt.text;   
+        nyt_png_exporter.transparency = nyt_png_exporter.dlg.transPnl.transparentChk.value; 
+        nyt_png_exporter.png8         = nyt_png_exporter.dlg.transPnl.png8Btn.value;
+        nyt_png_exporter.png24        = nyt_png_exporter.dlg.transPnl.png24Btn.value;
+
+        nyt_png_exporter.run_export();   
+        
+    };
+
+    this.dlg.show();
+
+    // end dialog
+  },
+
+
+ // run_export function. does the dirty work
+
+ run_export: function() {
+
+
+    
+    var num_exported = 0;
+    //var num_artboards = hideAllLayers(); 
+    
+    var options;
+    
+    
+    
+    if ( this.png8 ) {
+      options = new ExportOptionsPNG8();
+      options.antiAliasing = true;
+      options.transparency = this.transparency; 
+      options.artBoardClipping = true;
+        
+    } else {
+      options = new ExportOptionsPNG24();
+      options.antiAliasing = true;
+      options.transparency = this.transparency; 
+      options.artBoardClipping = true;
+    }
+
+    
+    var n = docRef.layers.length, background = docRef.layers["laptop background"];
+    background.visible = true;
+    for(var i=0, j = 1; i<n; ++i) {
+      
+        //this.hide_all_layers();
+        
+        var layer = docRef.layers[i];
+        if (!layer.locked || contains(layersToExclude, layer.name)) {
+            continue;
+        }
+        //docRef.activeLayer = docRef.layers.getByName(layer.name);
+        var lyr_name = j; j++;
+        
+        //if ( ! ( lyr_name.match( /^\+/ ) || lyr_name.match( /nyt_png_info/ ) || lyr_name.match( /^\-/) || lyr_name.match( /^Layer / ) )) {
+
+            layer.visible = true;
+	    
+            var destFile = new File( this.base_path + "/" + this.prefix + lyr_name + ".png");   
+            var export_type = this.png8 ? ExportType.PNG8 : ExportType.PNG24;
+
+            docRef.exportFile(destFile,export_type,options);
+	    
+            num_exported++;
+
+            this.dlg.progLabel.text = 'Exported ' + num_exported + ' of ' + this.num_to_export;
+            this.dlg.progBar.value = num_exported / this.num_to_export * 100;
+            this.dlg.update();        
+        //}
+        if (layer == background) {
+            break;
+        } else {
+            layer.visible = false;
+        }
+     }
+
+    
+    this.prefs_xml.nyt_path = this.base_path;
+    this.prefs_xml.nyt_prefix = this.prefix;
+    this.prefs_xml.nyt_transparency = this.transparency;
+    this.prefs_xml.nyt_png8 = this.png8;
+    this.prefs_xml.nyt_png24 = this.png24;
+    
+    
+    this.png_info_lyr.textFrames[0].contents = this.prefs_xml.toXMLString();
+    this.dlg.close();
+
+    //end run_export
+  },
+
+ 
+
+ hide_all_layers: function() {
+    return layersToExport;
+    var export_count = 0;
+
+    var n = docRef.layers.length;
+    for(var i=0; i<n; ++i) {
+      
+      layer = docRef.layers[i];
+      
+      lyr_name = layer.name;
+      
+      // any layers that start with + are always turned on
+      if ( lyr_name.match( /^\+/ ) ) {
+	layer.visible = true;
+	
+        // any layers that start with -, have default layer name, or named "nyt_png_info" are skipped
+      } else if ( lyr_name.match( /nyt_png_info/ ) || lyr_name.match( /^Layer /) || lyr_name.match( /^\-/ ) ){
+	layer.visible = false;
+	
+        // everything else we should export
+      } else {
+	layer.visible = false;
+	export_count++;
+      }
+    }
+    return export_count;
+  },
+
+ show_all_layers: function() {
+    
+    var n = docRef.layers.length;
+    for(var i=0; i<n; ++i) {
+      layer = docRef.layers[i];
+      layer.visible = true;
+    }
+  },
+
+}
+
+function contains(array, check) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == check) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function countUnlockedLayers() {
+    var count = 0;
+    for (var i = 0; i < docRef.layers.length; i++) {
+        if (!docRef.layers[i].locked) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+nyt_png_exporter.init();
+
+
+
---
+++ Winterbreak 2019/DennisQiuRoomProject1/images/laptopKeyboard.jsx
@@ -0,0 +1,184 @@
+﻿
+function createKeyboard() {
+    var alphabet = [["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], []];
+    var specials = [["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",  "{", "}", "|", ":", '"', "<", ">", "?", "~"], ["-", "=", "[", "]", "\\", ";", "'", ",", ".", "/", "`"]];
+    var phrases = [["PrtSc", "Pause", "Num"], ["SysRq", "Break", "Lock"], ["Pg", "Up", "Dn"], ["Tab", "Caps Lock", "Shift", "Ctrl", "Enter", "Backspace", "Esc", "Ins", "Del", "Home", "End"], ["Alt", "Gr"], ["Fn"], ["Scr Lk"]];
+    var fs = new Array(12), numbers = new Array(10);
+    for (var i = 0; i < fs.length; i++) {
+        fs[i] = "F" + (i+1);
+        if (i < numbers.length) {
+            numbers[i] = i;
+        }
+    }
+    fs.unshift(fs.pop());
+    fs = [[fs], []];
+    numbers.push(numbers.shift());
+    numbers = [[], [numbers]];
+    return organizeKeyboard([numbers, specials, phrases, alphabet, fs]);
+}
+
+function organizeKeyboard(keyboard) {
+    var topBottom = null;
+    for (var i = 0; i < keyboard.length; i++) {
+        if (topBottom == null) {
+            topBottom = {
+                organize: [[], []], items: 0, printOrganize: "",
+                getOrganize: function() {
+                    return this.organize;
+                },
+                execute: function(i) {
+                    var o = this.organize, j = this.items, p = this.printOrganize;
+                    for (var k = 0; k < o.length; j++, k++) {
+                         if (i == 3) {
+                            keyboard[i][j].push(o[k].pop());
+                        } else if (i == 4) {
+                            o[k].splice(getIndex (o[k], "Num")+1, 0, 9, 3, "0", ".", 7, 1, keyboard[i][j], 8, 6, 4, 2, 5, "/", "*", "-", "+");
+                            break;
+                        }
+                        o[k] = o[k].concat(keyboard[i][j]);
+                        var m = keyboard[i][j].length;
+                        if (j == 4) {
+                            keyboard[i].splice(j+1, 0, keyboard[i][j].slice(m-4, m));
+                        } else if (j > 4 && m > 1) {
+                            k++;
+                        }
+                    }
+                    for (var n = 0; n < o.length; n++) {
+                        p += o[n].toString() + "\n";
+                    }
+                    //print(p);
+                    if (i == 2) {
+                        var donePhrases = j == keyboard[i].length, arrange = keyboard[i][j];
+                        this.items = (donePhrases) ? 0 : j;
+                        if (donePhrases) {
+                                return;
+                        } else {
+                            if (arrange.length == 3) {
+                                var a = arrange.shift();
+                                for (var k = 0; k < arrange.length; k++) {
+                                    arrange[k] = a + " " + arrange[k];
+                                }
+                                keyboard[i].splice(k, 0, arrange);
+                             } else if (arrange.length == 2) {
+                                arrange[1] = arrange[0] + " " + arrange[1];
+                             }
+                         }
+                    }
+                }
+            }
+        }
+        topBottom.execute(i);
+        if (i == 2) {
+            for (var j = 0; j < 4; j++)
+                topBottom.execute(i);
+            }
+        }
+    return checkKeyboard(topBottom.getOrganize());
+}
+
+function getIndex(array, item) {
+   for (var i = 0; i < array.length; i++) {
+       if (array[i] == item) {
+           return i;
+       }
+   }
+   return -1;
+}
+
+function checkKeyboard(keyboard) {
+    for (var i = 0; i < keyboard.length; i++) {
+        var j = keyboard[i], m = [];
+        while (j.length > 0) {
+            var k = j.shift();
+            if (k instanceof Array) {
+                var n = k.shift();
+                if (n instanceof Array) {
+                    while (n.length > 0) {
+                        m = checkArray(m, n.shift());
+                    }
+                } else {
+                    while (k.length > 0) {
+                        m = checkArray(m, n); n = k.shift();
+                    }
+                    m = checkArray(m, n);
+                }
+            }
+            if (k != "") {
+                m = checkArray(m, k); 
+            }
+        }
+        keyboard[i] = m; //print();
+    }
+    return keyboard;
+}
+
+
+function checkArray(m, n) {
+    //print(n); 
+    m.push(n); return m;
+}
+
+function laptopKeyboard() {
+    var keyboard = createKeyboard(), top = keyboard[0], bottom = keyboard[1], isCenter = false, newLayer = null;
+    var color = {
+        setColor: function(array) {
+            return (array.length == 0) ? "blue" : "default";
+        }
+    }
+    while (top.length > 0 || bottom.length > 0) {
+        if (newLayer != null) {
+            newLayer.visible = false;
+            newLayer = null;
+        }
+        var t = top.shift(), b = (bottom.length > 0) ? bottom.shift() : "";
+        newLayer = doc.layers.add();
+        newLayer.name = t + ((b != "") ? " -> " + b : "");
+        createKey([t, b], [color.setColor(top), color.setColor(bottom)]);
+        var set = "Text"
+        app.doScript ("FontType", set);
+        if (isCenter) {
+            isCenter = t != "A";
+            if (isCenter) {
+                app.doScript ("Center", set);
+            }
+        } else {
+            isCenter = b == "`" || t == "Z";
+        }
+        newLayer.locked = true;
+    }
+}
+
+function createKey(text, color) {
+    for (var i = 0, y = -450; i < 2; i++, y -= 150) {
+        var t = text[i];
+        if (t != "") {
+            var bounds = doc.pathItems.rectangle(y, 370, 500, 200);
+            var frame = doc.textFrames.areaText(bounds);
+            frame.contents = t;
+            var font = frame.textRange.characterAttributes;
+            font.fillColor = setColor(color[i]);
+            font.size = 120;
+        }
+    }
+}
+
+function createColor(r, g, b) {
+    var color = new RGBColor();
+    color.red = r; color.green = g; color.blue = b;
+    return color;
+}
+
+function setColor(setColor) {
+    var color;
+    if (setColor == "default") {
+        color = createColor(255, 255, 255);
+    } else if (setColor == "blue") {
+        color = createColor(70, 130, 180);
+    } else {
+        throw new Error("Variable 'setColor = " + setColor + "' must be either 'default' or 'blue'.");
+    }
+    return color;
+}
+
+var doc = app.activeDocument;
+laptopKeyboard();
